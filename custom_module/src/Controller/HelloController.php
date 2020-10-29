971e10e7a1010f7b32f9df0bcb82617ccab3fe5d
knvc <koenvancrombrugge@outlook.com>
2020-10-29 20:10:10 +0100

    added redirect controller to twig template

--- custom_module/src/Controller/HelloController.php
+++ custom_module/src/Controller/HelloController.php
@@ -4,11 +4,25 @@
  * Contains \Drupal\custom_module\Controller\HelloController.
  */
 namespace Drupal\custom_module\Controller;
+
 class HelloController {
+
   public function content() {
-    return array(
-      '#type' => 'markup',
-      '#markup' => t('Hello, World!'),
-    );
+
+    // Do something with your variables here.
+    $myText = 'This is not just a default text!';
+    $myNumber = 1;
+    $myArray = [1, 2, 3];
+ 
+    return [
+       // Your theme hook name.
+       '#theme' => 'module_name_theme_hook',      
+       // Your variables.
+       '#variable1' => $myText,
+       '#variable2' => $myNumber,
+       '#variable3' => $myArray,
+     ];
+
   }
+  
 }
